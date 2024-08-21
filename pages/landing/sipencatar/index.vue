9c660215271224a08d04a95987ae968a422ee526
yogiwirmanda <wirmanda@gmail.com>
2024-08-21 09:53:57 +0700

    update dynamic form

--- pages/landing/sipencatar/index.vue
+++ pages/landing/sipencatar/index.vue
@@ -5,12 +5,15 @@ import { useRouter } from "vue-router";
 import "vue-select/dist/vue-select.css";
 import vSelect from "vue-select";
 import { school } from "../data/school";
+import { FormKitSchema } from "@formkit/vue";
 
 const submitted = ref(false);
 const openModal = ref(false);
 const eventDetail = ref({});
 const router = useRouter();
 const school_from = ref("");
+const formScheme = ref([]);
+const formData = ref(null);
 
 const loadData = async () => {
   let response = await useCustomFetch("api/event/slug/test-event", "get", {}, true);
@@ -18,17 +21,24 @@ const loadData = async () => {
 };
 
 const loadForm = async () => {
-  let responseForm = await useCustomFetch("api/event-content/detail/25/form_register", "get", {}, true);
-  console.log(responseForm);
+  let responseForm = await useCustomFetch(
+    "api/event-content/detail/25/form_register",
+    "get",
+    {},
+    true
+  );
+  formScheme.value = JSON.parse(responseForm.data.value.data.value);
+  console.log(formScheme.value);
 };
 
 loadData();
 loadForm();
 
-const submitHandler = async (formData) => {
-  let payload = formData;
-  payload.event_id = eventDetail?.value?.id;
+const submitHandler = async () => {
+  let payload = formData.value;
+  payload.event_id = eventDetail?.value?.id || 25;
   payload.school_from = school_from.value.label;
+  console.log(payload);
   setTimeout(async () => {
     let response = await useCustomFetch(
       "api/contact/manual-invitation",
@@ -38,8 +48,6 @@ const submitHandler = async (formData) => {
     );
     router.push("/detail-" + response.data.value.data.uid);
   }, 1000);
-  // submitted.value = true;
-  // openModal.value = true;
 };
 
 const closeModal = () => {
@@ -63,75 +71,8 @@ const closeModal = () => {
           <h1 class="text-4xl font-bold mb-5 mt-2">Sosialisasi SIPENCATAR 2024</h1>
           <h1 class="text-2xl font-medium mb-5">Form Registrasi</h1>
         </div>
-        <FormKit
-          type="form"
-          id="registration-example"
-          :form-class="submitted ? 'hide' : 'show'"
-          submit-label="Register"
-          @submit="submitHandler"
-          :actions="false"
-        >
-          <FormKit
-            type="text"
-            name="name"
-            label="Nama"
-            placeholder="Masukkan Nama"
-            validation="required"
-          />
-          <div class="grid grid-cols-2 gap-5">
-            <FormKit
-              type="text"
-              name="place_of_birth"
-              label="Tempat Lahir"
-              placeholder="Masukkan Tempat Lahir"
-              validation="required"
-            />
-            <FormKit
-              type="date"
-              name="date_of_birth"
-              value="1999-01-01"
-              label="Tanggal Lahir"
-              validation="required|date_before:2018-01-01"
-              validation-visibility="live"
-            />
-          </div>
-          <FormKit
-            type="email"
-            label="E-mail"
-            name="email"
-            validation="required|email"
-            placeholder="xxx@gmail.com"
-          />
-          <FormKit
-            type="tel"
-            label="No Whatsapp"
-            name="phone"
-            placeholder="081217xxxxxx"
-            validation-visibility="dirty"
-          />
-          <div>
-            <label
-              class="block text-neutral-700 text-sm font-bold !inline-flex mb-1 formkit-label"
-              for="input_4"
-              >Asal Sekolah</label
-            >
-            <v-select
-              value="label"
-              label="label"
-              name="school_from"
-              v-model="school_from"
-              placeholder="Pilih Asal Sekolah"
-              :options="school"
-              class="rounded rounded-md mb-4"
-            ></v-select>
-          </div>
-          <FormKit
-            name="profession"
-            type="radio"
-            label="Profesi Anda"
-            :options="['Guru', 'Siswa']"
-          />
-          <FormKit type="submit" label="Daftar" />
+        <FormKit type="form" v-model="formData" @submit="submitHandler">
+          <FormKitSchema :schema="formScheme" />
         </FormKit>
       </div>
     </div>
@@ -158,5 +99,8 @@ const closeModal = () => {
   --tw-border-opacity: 1;
   border-color: rgb(163 163 163 / 1);
   border-color: rgb(163 163 163 / var(--tw-border-opacity));
+}
+.formkit-label {
+  color: black !important;
 }
 </style>
