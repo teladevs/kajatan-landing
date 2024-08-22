5066346f485d7850817150f227de293bf5217aef
yogiwirmanda <wirmanda@gmail.com>
2024-08-22 18:15:09 +0700

    try update subdomain host

--- server/middleware/subdomain.ts
+++ server/middleware/subdomain.ts
@@ -1,13 +1,11 @@
 export default defineEventHandler(({ req, res, context }) => {
-  const hostname = req.headers.host || "landing.kajatan.telanusa.id"
+  const hostname = req.headers.host || "telanusa.id"
 
-  const mainDomain = ["localhost:3000", "landing.kajatan.telanusa.id"]
+  const mainDomain = ["localhost:3000", "telanusa.id"]
 
   if (!mainDomain.includes(hostname)) {
     const currentHost =
-      process.env.NODE_ENV === "production"
-        ? hostname.replace(`.landing.kajatan.telanusa.id`, "")
-        : hostname.replace(`.localhost:3000`, "")
+        hostname.replace(`.telanusa.id`, "")
 
     console.log({ currentHost })
     context.subdomain = currentHost
