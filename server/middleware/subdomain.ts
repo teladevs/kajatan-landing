b5dd7ef04041ddd5f66c9985e8fe5249d7b0d6dc
yogiwirmanda <wirmanda@gmail.com>
2024-08-22 18:28:15 +0700

    update middleware

--- server/middleware/subdomain.ts
+++ server/middleware/subdomain.ts
@@ -1,13 +1,15 @@
 export default defineEventHandler(({ req, res, context }) => {
-  const hostname = req.headers.host || "telanusa.id"
+  const hostname = req.headers.host || "landing.kajatan.telanusa.id"
 
-  const mainDomain = ["localhost:3000", "telanusa.id"]
+  const mainDomain = ["localhost:3000", "landing.kajatan.telanusa.id"]
 
+  console.log({ hostname })
   if (!mainDomain.includes(hostname)) {
     const currentHost =
-        hostname.replace(`.telanusa.id`, "")
+      process.env.NODE_ENV === "production"
+        ? hostname.replace(`.landing.kajatan.telanusa.id`, "")
+        : hostname.replace(`.localhost:3000`, "")
 
-    console.log({ currentHost })
     context.subdomain = currentHost
   }
 })
