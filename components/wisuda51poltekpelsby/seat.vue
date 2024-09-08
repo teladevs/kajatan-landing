0a5cdc199b56ad5c4876613880c90e47e8fbc23f
yogiwirmanda <wirmanda@gmail.com>
2024-09-08 13:13:04 +0700

    update seat

---
+++ components/wisuda51poltekpelsby/seat.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="seat-container">
+    <div class="grid grid-cols-3 gap-5 text-center">
+      <div v-for="(valSec, idxSec) in sectorAll">
+        <div class="text-center my-10">{{ valSec.name }}</div>
+        <div class="grid grid-cols-4 gap-2">
+          <div
+            class="seat flex items-center justify-center"
+            :id="`sector-${valSec.sector}-${idxSeat + 1}`"
+            v-for="(valSeat, idxSeat) in valSec.total"
+          >
+            {{ idxSeat + 1 }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+.seat {
+  width: 50px;
+  height: 50px;
+  background-color: gray;
+}
+.seat.booked {
+  width: 50px;
+  height: 50px;
+  background-color: red;
+}
+</style>
+<script setup>
+const sectorAll = [
+  { name: "Sector A", sector: "sca", total: 20 },
+  { name: "Sector B", sector: "scb", total: 10 },
+  { name: "Sector C", sector: "scc", total: 30 },
+];
+
+const seatBooked = [
+  "sector-sca-5",
+  "sector-sca-2",
+  "sector-sca-10",
+  "sector-scb-9",
+  "sector-scc-29",
+  "sector-scc-25",
+  "sector-scc-20",
+];
+
+const setSeatBooked = () => {
+  seatBooked.forEach((val, idx) => {
+    const element = document.querySelector(`#${val}`);
+    if (element) {
+      element.classList.add("booked");
+    }
+  });
+};
+
+setTimeout(() => {
+  setSeatBooked();
+}, 1500);
+</script>
--- pages/seat.vue
+++ pages/seat.vue
@@ -1,60 +1,29 @@
-<template>
-  <div class="seat-container">
-    <div class="grid grid-cols-3 gap-5 text-center">
-      <div v-for="(valSec, idxSec) in sectorAll">
-        <div class="text-center my-10">{{ valSec.name }}</div>
-        <div class="grid grid-cols-4 gap-2">
-          <div
-            class="seat flex items-center justify-center"
-            :id="`sector-${valSec.sector}-${idxSeat + 1}`"
-            v-for="(valSeat, idxSeat) in valSec.total"
-          >
-            {{ idxSeat + 1 }}
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</template>
-<style scoped>
-.seat {
-  width: 50px;
-  height: 50px;
-  background-color: gray;
-}
-.seat.booked {
-  width: 50px;
-  height: 50px;
-  background-color: red;
-}
-</style>
-<script setup>
-const sectorAll = [
-  { name: "Sector A", sector: "sca", total: 20 },
-  { name: "Sector B", sector: "scb", total: 10 },
-  { name: "Sector C", sector: "scc", total: 30 },
-];
+<script setup lang="ts">
+const url = useRequestURL();
+const loadComponent = ref<any>(null);
 
-const seatBooked = [
-  "sector-sca-5",
-  "sector-sca-2",
-  "sector-sca-10",
-  "sector-scb-9",
-  "sector-scc-29",
-  "sector-scc-25",
-  "sector-scc-20",
-];
-
-const setSeatBooked = () => {
-  seatBooked.forEach((val, idx) => {
-    const element = document.querySelector(`#${val}`);
-    if (element) {
-      element.classList.add("booked");
-    }
-  });
+const doLoadComponent = async (componentName: string) => {
+  loadComponent.value = defineAsyncComponent(
+    () => import(`@/components/${componentName}/seat.vue`)
+  );
 };
 
-setTimeout(() => {
-  setSeatBooked();
-}, 1500);
+const loadData = async () => {
+  if (url.host === "kajatan.telanusa.id" || url.host === "localhost:3000") {
+    doLoadComponent("default");
+  } else {
+    let response = await useCustomFetch(`api/event/domain/${url.host}`, "get", {}, true);
+    if (response.data.value.status) {
+      doLoadComponent(response.data.value.data.config.value.name);
+    } else {
+      loadComponent.value = defineAsyncComponent(() => import(`@/components/error.vue`));
+    }
+  }
+};
+
+loadData();
 </script>
+
+<template>
+  <component :is="loadComponent" />
+</template>
