dcf5c0c116d223beef91306063d4d873bc978baf
yogiwirmanda <wirmanda@gmail.com>
2024-09-11 19:50:17 +0700

    update link seat

--- components/wisuda51poltekpelsby/seat.vue
+++ components/wisuda51poltekpelsby/seat.vue
@@ -20,8 +20,7 @@
                 <dd
                   class="mt-1 text-sm leading-6 font-bold text-gray-100 sm:col-span-2 sm:mt-0"
                 >
-                  <!-- {{ detailContact.name }} -->
-                  Yogi Wirmanda
+                  {{ detailContact.name }}
                 </dd>
               </div>
               <div class="px-1 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
