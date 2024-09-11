bfce43c548020665ba8f002588416a7e36c5f999
yogiwirmanda <wirmanda@gmail.com>
2024-09-11 19:46:19 +0700

    update seat

--- components/wisuda51poltekpelsby/seat.vue
+++ components/wisuda51poltekpelsby/seat.vue
@@ -38,7 +38,7 @@
                 <dd
                   class="mt-1 text-sm leading-6 font-bold text-gray-100 sm:col-span-2 sm:mt-0"
                 >
-                  ZONA 2
+                  ZONA 2 & 3
                 </dd>
               </div>
               <div class="px-1 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
