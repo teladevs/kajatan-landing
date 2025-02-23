8f71949862c917651d66f22eaa64e0bd91f1147c
Yogi Wirmanda <wirmanda@gmail.com>
2025-02-23 17:27:47 +0700

    repair seat redirect

--- pages/seat.vue
+++ pages/seat.vue
@@ -4,7 +4,7 @@ const loadComponent = ref<any>(null);
 
 const doLoadComponent = async (componentName: string) => {
   loadComponent.value = defineAsyncComponent(
-    () => import(`@/components/wisuda51poltekpelsby/seat.vue`)
+    () => import(`@/components/wisuda52poltekpelsby/seat.vue`)
   );
 };
 
@@ -12,11 +12,18 @@ const loadData = async () => {
   if (url.host === "kajatan.telanusa.id" || url.host === "localhost:3000") {
     doLoadComponent("default");
   } else {
-    let response = await useCustomFetch(`api/event/domain/${url.host}`, "get", {}, true);
+    let response = await useCustomFetch(
+      `api/event/domain/${url.host}`,
+      "get",
+      {},
+      true
+    );
     if (response.data.value.status) {
       doLoadComponent(response.data.value.data.config.value.name);
     } else {
-      loadComponent.value = defineAsyncComponent(() => import(`@/components/error.vue`));
+      loadComponent.value = defineAsyncComponent(
+        () => import(`@/components/error.vue`)
+      );
     }
   }
 };
