b480faa9dd01ba585cdf68399e2218017db10e9e
Yogi Wirmanda <wirmanda@gmail.com>
2025-08-03 17:42:38 +0700

    update sponsored logo

--- components/gene51s17th/landing.vue
+++ components/gene51s17th/landing.vue
@@ -1,10 +1,10 @@
 <template>
   <NuxtLayout name="blank">
-    <div class="mx-auto px-7 py-5 mt-10 main-content">
+    <div class="mx-auto px-7 py-5 mt-10 main-content max-w-[500px]">
       <div class="mt-5 mb-5 text-slate-800 text-2xl font-bold text-center">
         Supported By
       </div>
-      <div class="w-full grid grid-cols-4 gap-5 flex items-center mt-5">
+      <div class="w-full grid grid-cols-2 gap-5 px-[80px]">
         <img
           src="/public/caaip/caaip-logo.jpg"
           alt="image-kemenhub"
@@ -15,6 +15,10 @@
           alt="image-kemenhub"
           class="logo-instansi"
         />
+      </div>
+      <div
+        class="w-full grid grid-cols-3 gap-5 flex items-center justify-center mt-5"
+      >
         <img
           src="/public/caaip/telanusa-logo.png"
           alt="image-kemenhub"
@@ -26,12 +30,12 @@
           class="logo-instansi"
         />
         <img
-          src="/public/caaip/intescomindo.jpeg"
+          src="/public/caaip/inwest.jpeg"
           alt="image-kemenhub"
           class="logo-instansi"
         />
         <img
-          src="/public/caaip/inwest.jpeg"
+          src="/public/caaip/intescomindo.jpeg"
           alt="image-kemenhub"
           class="logo-instansi"
         />
@@ -45,10 +49,13 @@
           alt="image-kemenhub"
           class="logo-instansi"
         />
-      </div>
-      <div class="grid grid-cols-3 gap-5 mt-5 flex items-center">
         <img
           src="/public/caaip/pelindo.jpeg"
+          alt="image-kemenhub"
+          class="logo-instansi"
+        />
+        <img
+          src="/public/caaip/pelindo2.png"
           alt="image-kemenhub"
           class="logo-instansi"
         />
@@ -63,12 +70,17 @@
           class="logo-instansi"
         />
       </div>
+      <div class="w-full text-center mt-[50px]">
+        <div class="text-md text-slate-800 font-semibold">Organized by</div>
+        <div class="text-md">Harmoni Organizer</div>
+        <div class="text-md">PT Manawa Cipta Harmoni</div>
+      </div>
     </div>
   </NuxtLayout>
 </template>
 <style>
 .logo-instansi {
-  width: 150px !important;
+  width: 100px !important;
   height: auto;
 }
 </style>
