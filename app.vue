b87f476c70ff68e4181e7cfe2f2f9facdf501a2e
Yogi Wirmanda <wirmanda@gmail.com>
2025-08-19 13:57:55 +0700

    Update app.vue

--- app.vue
+++ app.vue
@@ -6,7 +6,7 @@
 :root {
   --52primary: #770808;
   --52primaryDarker: #5a0808;
-  --53primary: #941b20;
-  --53primaryDarker: #941b20;
+  --53primary: #ba1219;
+  --53primaryDarker: #660b0e;
 }
 </style>
