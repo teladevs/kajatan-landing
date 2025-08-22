d519cd495039b25e67956ca0baa9fb3362a55428
Yogi Wirmanda <wirmanda@gmail.com>
2025-08-23 01:29:56 +0700

    Update simulation.vue

--- components/wisuda53poltekpelsby/simulation.vue
+++ components/wisuda53poltekpelsby/simulation.vue
@@ -59,15 +59,15 @@
   color: white;
   font-size: 10px;
 }
-.seat.booked.YATIM {
+.booked.YATIM {
   background-color: #ffc0cb;
   color: black;
 }
-.seat.booked.VIP-VVIP {
+.booked.VIP-VVIP {
   background-color: #ffffff;
   color: black;
 }
-.booked.left {
+/* .booked.DP-I {
   background-color: #b3d5e7;
   color: black;
 }
@@ -77,6 +77,58 @@
 }
 .booked.center {
   background-color: #6b3063;
+  color: white;
+} */
+.booked.DP-I {
+  background-color: #0000ff;
+  color: white;
+}
+.booked.DP-II {
+  background-color: #000080;
+  color: white;
+}
+.booked.DP-III {
+  background-color: #ff0000;
+  color: white;
+}
+.booked.DP-IV {
+  background-color: #add8e6;
+  color: black;
+}
+.booked.DP-V {
+  background-color: #00ff00;
+  color: black;
+}
+.booked.DP-ETO {
+  background-color: #e98a07;
+  color: black;
+}
+.booked.TROK {
+  background-color: #ffff00;
+  color: black;
+}
+.booked.TRPK {
+  background-color: #ffa500;
+  color: white;
+}
+.booked.TRKK {
+  background-color: #800080;
+  color: white;
+}
+.booked.TRANSLA {
+  background-color: #ffc0cb;
+  color: black;
+}
+.booked.D-III-NAUTIKA {
+  background-color: #8b4513;
+  color: white;
+}
+.booked.D-III-TEKNIKA {
+  background-color: #a00e5a;
+  color: white;
+}
+.booked.D-III-ETO {
+  background-color: #008080;
   color: white;
 }
 </style>
@@ -95,144 +147,89 @@ const loadData = async () => {
 };
 
 const sectorAll = [
-  { name: "SEKTOR 1", sector: 1, total: 160, row: 16, column: 10 },
-  { name: "SEKTOR 2", sector: 2, total: 160, row: 16, column: 10 },
-  { name: "SEKTOR 3", sector: 3, total: 160, row: 16, column: 10 },
-  { name: "SEKTOR 4", sector: 4, total: 160, row: 16, column: 10 },
-  { name: "SEKTOR 5", sector: 5, total: 48, row: 16, column: 3 },
-  { name: "SEKTOR 6", sector: 6, total: 48, row: 16, column: 3 },
-  { name: "SEKTOR 7", sector: 7, total: 160, row: 16, column: 10 },
-  { name: "SEKTOR 8", sector: 8, total: 160, row: 16, column: 10 },
-  { name: "SEKTOR 9", sector: 9, total: 160, row: 16, column: 10 },
-  { name: "SEKTOR 10", sector: 10, total: 160, row: 16, column: 10 },
+  { name: "SEKTOR 1", sector: 1, total: 170, row: 15, column: 10 },
+  { name: "SEKTOR 2", sector: 2, total: 170, row: 15, column: 10 },
+  { name: "SEKTOR 3", sector: 3, total: 170, row: 15, column: 10 },
+  { name: "SEKTOR 4", sector: 4, total: 100, row: 10, column: 10 },
+  { name: "SEKTOR 5", sector: 5, total: 100, row: 10, column: 10 },
+  { name: "SEKTOR 6", sector: 6, total: 100, row: 10, column: 10 },
+  { name: "SEKTOR 7", sector: 7, total: 100, row: 10, column: 10 },
+  { name: "SEKTOR 8", sector: 8, total: 170, row: 15, column: 10 },
+  { name: "SEKTOR 9", sector: 9, total: 170, row: 15, column: 10 },
+  { name: "SEKTOR 10", sector: 10, total: 170, row: 15, column: 10 },
 ];
 
 const checkAlphabet = (number, seat, sector) => {
   let alphabet = "";
-  if (sector == 5 || sector == 6) {
-    switch (number) {
-      case 0:
-        alphabet = "A";
-        break;
-      case 3:
-        alphabet = "B";
-        break;
-      case 6:
-        alphabet = "C";
-        break;
-      case 9:
-        alphabet = "D";
-        break;
-      case 12:
-        alphabet = "E";
-        break;
-      case 15:
-        alphabet = "F";
-        break;
-      case 18:
-        alphabet = "G";
-        break;
-      case 21:
-        alphabet = "H";
-        break;
-      case 24:
-        alphabet = "I";
-        break;
-      case 27:
-        alphabet = "J";
-        break;
-      case 30:
-        alphabet = "K";
-        break;
-      case 33:
-        alphabet = "L";
-        break;
-      case 36:
-        alphabet = "M";
-        break;
-      case 39:
-        alphabet = "N";
-        break;
-      case 42:
-        alphabet = "O";
-        break;
-      case 45:
-        alphabet = "P";
-        break;
+  switch (number) {
+    case 0:
+      alphabet = "A";
+      break;
+    case 10:
+      alphabet = "B";
+      break;
+    case 20:
+      alphabet = "C";
+      break;
+    case 30:
+      alphabet = "D";
+      break;
+    case 40:
+      alphabet = "E";
+      break;
+    case 50:
+      alphabet = "F";
+      break;
+    case 60:
+      alphabet = "G";
+      break;
+    case 70:
+      alphabet = "H";
+      break;
+    case 80:
+      alphabet = "I";
+      break;
+    case 90:
+      alphabet = "J";
+      break;
+    case 100:
+      alphabet = "K";
+      break;
+    case 110:
+      alphabet = "L";
+      break;
+    case 120:
+      alphabet = "M";
+      break;
+    case 130:
+      alphabet = "N";
+      break;
+    case 140:
+      alphabet = "O";
+      break;
+    case 150:
+      alphabet = "P";
+      break;
+    case 160:
+      alphabet = "Q";
+      break;
+    case 170:
+      alphabet = "R";
+      break;
+    case 180:
+      alphabet = "S";
+      break;
 
-      default:
-        break;
-    }
-  } else {
-    switch (number) {
-      case 0:
-        alphabet = "A";
-        break;
-      case 10:
-        alphabet = "B";
-        break;
-      case 20:
-        alphabet = "C";
-        break;
-      case 30:
-        alphabet = "D";
-        break;
-      case 40:
-        alphabet = "E";
-        break;
-      case 50:
-        alphabet = "F";
-        break;
-      case 60:
-        alphabet = "G";
-        break;
-      case 70:
-        alphabet = "H";
-        break;
-      case 80:
-        alphabet = "I";
-        break;
-      case 90:
-        alphabet = "J";
-        break;
-      case 100:
-        alphabet = "K";
-        break;
-      case 110:
-        alphabet = "L";
-        break;
-      case 120:
-        alphabet = "M";
-        break;
-      case 130:
-        alphabet = "N";
-        break;
-      case 140:
-        alphabet = "O";
-        break;
-      case 150:
-        alphabet = "P";
-        break;
-
-      default:
-        break;
-    }
+    default:
+      break;
   }
 
-  if (sector >= 2 && sector <= 4) {
+  if (sector >= 2) {
     return alphabet + (seat + sector * 10 - 10);
   }
 
-  if (sector > 6) {
-    return alphabet + (seat + (sector * 10 - 24));
-  }
-
-  if (sector == 5) {
-    return alphabet + (seat + 40);
-  }
-
-  if (sector == 6) {
-    return alphabet + (seat + 43);
+  if (sector == 1) {
+    return alphabet + seat;
   }
 
   return alphabet + seat;
