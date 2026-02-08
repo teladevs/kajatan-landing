005ee52b975853882e09af067b4bb0d52ee0415b
Yogi Wirmanda <wirmanda@gmail.com>
2026-02-08 22:13:14 +0700

    update denah lapangan 54 sby

--- components/wisuda54poltekpelsby/index.vue
+++ components/wisuda54poltekpelsby/index.vue
@@ -486,7 +486,7 @@
 </template>
 <script setup>
 import marsPerhubungan from "@/public/wisuda52poltekpelsby/mars-perhubungan.mp3";
-import layoutUpacara from "@/public/wisuda53poltekpelsby/w53-lapangan_upacara_mobile.webp";
+import layoutUpacara from "@/public/wisuda54poltekpelsby/p2s-w54-layout-upacara-inv.webp";
 import layoutTempatDuduk from "@/public/wisuda53poltekpelsby/w53-layout_seat_mobile.webp";
 import denahParkir from "@/public/wisuda53poltekpelsby/w53-denah_parkir.webp";
 import AudioPlayer from "@/components/audio";
