2662362fd73adedf83b64c83f2c083bd43df3b6b
Yogi Wirmanda <wirmanda@gmail.com>
2026-08-03 17:15:44 +0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -23,7 +23,7 @@ const loadData = async () => {
       if (template == null || template == undefined) {
         template = response.data.value.data.config.value.name;
       }
-      doLoadComponent(template);
+      doLoadComponent("bonvoyagebanten");
     } else {
       loadComponent.value = defineAsyncComponent(
         () => import(`@/components/error.vue`),
