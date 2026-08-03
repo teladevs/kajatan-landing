51dbb3213bdf2a105a2f61bce6f66d08a9e2df3e
Yogi Wirmanda <wirmanda@gmail.com>
2026-08-03 16:37:48 +0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,7 @@ const doLoadComponent = async (componentName: string) => {
 
 const loadData = async () => {
   if (url.host === "landing.kajatan.com" || url.host === "localhost:3000") {
-    doLoadComponent("bonvoyagebanten");
+    doLoadComponent("default");
   } else {
     let response = await useCustomFetch(
       `api/event/domain/${url.host}`,
