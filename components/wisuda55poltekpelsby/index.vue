e396fb83a7d9a6b305b6906947145c8fd648b08d
Yogi Wirmanda <wirmanda@gmail.com>
2026-09-14 19:31:14 +0700

    update index components

--- components/wisuda55poltekpelsby/index.vue
+++ components/wisuda55poltekpelsby/index.vue
@@ -625,7 +625,7 @@ const detailContact = ref("Politeknik Pelayaran Surabaya");
 const loadData = async () => {
   if (router.currentRoute.value.query.id != undefined) {
     let eventDetail = await useCustomFetch(
-      `api/event/domain/${"wisuda53poltekpelsby.telanusa.com"}`,
+      `api/event/domain/${"wisuda55poltekpelsby.telanusa.com"}`,
       "get",
       {},
       true,
